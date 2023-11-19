6f4e766ff2c40ae863cfa828239091a579e917ba
Goldenmarquice <usmcmarquice@gmail.com>
2023-11-18 17:17:54 -0800

    Final

--- FrontEnd/src/pages/about.jsx
+++ FrontEnd/src/pages/about.jsx
@@ -5,47 +5,44 @@ function About() {
     return (
         <div className="about page">
             <h1>About Us Page</h1>
-            <p>Some text about who we are and what we do.</p>
-            <p>Resize the browser window to see that this page is responsive by the way.</p>
+            <p> We are the team for Quick Bets</p>
         
             <h2 style={{ textAlign: 'center' }}>Our Team</h2>
         
-            <div className="row">
+            <div className="row" style={{ textAlign: 'center' }}>
                 <div className="column">
                     <div className="card">
-                        <img src="" alt="Marquice" style={{ width: '100%' }} />
+                        
                         <div className="container">
+                        <img src="/img/marquice.jpeg" alt="Marquice"  />
                             <h2>Marquice Bowman</h2>
                             <p className="title">Software Developer</p>
-                            <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                            <p>Marquice@example.com</p>
-                            <p><button className="button">Contact</button></p>
+                            <p>bowman.marquice@gmail.com</p>
+                            <p><a href="https://github.com/GoldenMarquice"><button className="button">Contact</button></a></p>
                         </div>
                     </div>
                 </div>
             
                 <div className="column">
                     <div className="card">
-                        <img src="" alt="Brandon Lile" style={{ width: '100%' }} />
                         <div className="container">
+                        <img src="/img/me-braids.jpg" alt="Brandon Lile"  />
                             <h2>Brandon Lile</h2>
                             <p className="title">Software Developer</p>
-                            <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                            <p>Brandon@example.com</p>
-                            <p><button className="button">Contact</button></p>
+                            <p>detroitking04@gmail.com</p>
+                            <p><a href="https://github.com/BrandonL86"><button className="button">Contact</button></a></p>
                         </div>
                     </div>
                 </div>
             
                 <div className="column">
                     <div className="card">
-                        <img src="" alt="Adam" style={{ width: '100%' }} />
+                        <img src="/img/adam.jpeg" alt="Adam"  />
                         <div className="container">
                             <h2>Adam Young</h2>
                             <p className="title">Software Developer</p>
-                            <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                            <p>Adam@example.com</p>
-                            <p><button className="button">Contact</button></p>
+                            <p>Adam.young@sdgku.edu</p>
+                            <p><a href="https://github.com/conchaking"><button className="button">Contact</button></a></p>
                         </div>
                     </div>
                 </div>
